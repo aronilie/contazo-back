67e3fcfa44b92181c5e1ff8df23a860afc59b65c
Aron Ilie <aronilie.code@gmail.com>
2022-08-29 19:34:04 +0200

    Created generalError function

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,8 +1,28 @@
 import "../loadEnvironment";
-import { Request, Response } from "express";
+import Debug from "debug";
+import chalk from "chalk";
+import { NextFunction, Request, Response } from "express";
+import CustomError from "./CustomError";
+
+const debug = Debug("utils:errors");
 
 const notFoundError = (req: Request, res: Response) => {
   res.status(404).json({ error: "Oops! Page not found :(" });
+};
+
+export const middleweareGeneralError = (
+  error: CustomError,
+  req: Request,
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  next: NextFunction
+) => {
+  const errorCode = error.statusCode ?? 500;
+  const errorMessage = error.publicMessage ?? "Something went wrong :(";
+
+  debug(chalk.bgRed.white(error.message));
+
+  res.status(errorCode).json({ error: errorMessage });
 };
 
 export default notFoundError;
