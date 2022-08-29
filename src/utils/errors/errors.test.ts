2146da8cdbb326686369be77cc72c1e8e328a374
Aron Ilie <aronilie.code@gmail.com>
2022-08-29 20:59:10 +0200

    Tested generalError function

--- src/utils/errors/errors.test.ts
+++ src/utils/errors/errors.test.ts
@@ -1,8 +1,10 @@
-import { Request, Response } from "express";
-import { notFoundError } from "./errors";
+import { NextFunction, Request, Response } from "express";
+import CustomError from "../CustomError/CustomError";
+import { generalError, notFoundError } from "./errors";
 
 let mockRequest: Partial<Request>;
 let mockResponse: Partial<Response>;
+const nextFunction: NextFunction = jest.fn();
 
 beforeEach(() => {
   mockRequest = {};
@@ -17,7 +19,34 @@ describe("Given notFoundError function", () => {
       const expectedResponse = {
         error: "Oops! Page not found :(",
       };
+
       notFoundError(mockRequest as Request, mockResponse as Response);
+
+      expect(mockResponse.json).toBeCalledWith(expectedResponse);
+    });
+  });
+});
+
+describe("Given generalError function", () => {
+  describe("When it is instantiated", () => {
+    test("Then it should give a response with the error message 'Oops! Page not found :('", async () => {
+      const error: CustomError = {
+        publicMessage: "Something went wrong :(",
+        code: "",
+        message: "",
+        name: "",
+        privateMessage: "",
+        statusCode: 500,
+      };
+
+      const expectedResponse = { error: error.publicMessage };
+
+      generalError(
+        error,
+        mockRequest as Request,
+        mockResponse as Response,
+        nextFunction
+      );
 
       expect(mockResponse.json).toBeCalledWith(expectedResponse);
     });
