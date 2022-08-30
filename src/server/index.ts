aed2d2636b924d7af3d90313418d2f1a32e496c0
Aron Ilie <aronilie.code@gmail.com>
2022-08-30 17:16:52 +0200

    Created usersRouter router

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,12 +2,15 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import { notFoundError, generalError } from "../utils/errors/errors";
+import usersRouter from "./routers/usrersRouter";
 
 const app = express();
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use("/users", usersRouter);
 
 app.use(notFoundError);
 app.use(generalError);
