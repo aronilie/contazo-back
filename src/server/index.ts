956548951965a9081c84027c4a9b23818701aa56
Aron Ilie <aronilie.code@gmail.com>
2022-08-30 17:48:41 +0200

    Moved all middleweares from server index to src index

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,18 +1,5 @@
 import express from "express";
-import cors from "cors";
-import morgan from "morgan";
-import { notFoundError, generalError } from "../utils/errors/errors";
-import usersRouter from "./routers/usrersRouter";
 
 const app = express();
-
-app.use(cors());
-app.use(morgan("dev"));
-app.use(express.json());
-
-app.use("/users", usersRouter);
-
-app.use(notFoundError);
-app.use(generalError);
 
 export default app;
