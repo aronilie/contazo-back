ecb7f8789cebf8eebda32e6b387b6488dbddb769
Aron Ilie <aronilie.code@gmail.com>
2022-08-31 20:51:03 +0200

    Tested usersRouter router

---
+++ src/server/routers/usersRouter/usersRouter.test.ts
@@ -0,0 +1,34 @@
+import request from "supertest";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import app from "../..";
+import connectDB from "../../../database/index";
+
+let server: MongoMemoryServer;
+
+beforeAll(async () => {
+  server = await MongoMemoryServer.create();
+  const mongoURL = server.getUri();
+
+  await connectDB(mongoURL);
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  await server.stop();
+});
+
+describe("Given the POST endpoint /users/register", () => {
+  describe("When it receives a request with username 'Dan' and password '+89 258 21 55 84'", () => {
+    test("Then it should response with status 201 and the user data", async () => {
+      const user = { phoneNumber: "+65 782 22 45", password: "admin" };
+      const expectedStatus = 201;
+
+      const response = await request(app)
+        .post("/users/register")
+        .send(user)
+        .expect(expectedStatus);
+      expect(response.statusCode).toEqual(expectedStatus);
+    });
+  });
+});
--- src/server/routers/usersRouter/usersRouter.ts
+++ src/server/routers/usersRouter/usersRouter.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import registerUser from "../controllers/usersController/usersController";
+import registerUser from "../../controllers/usersController/usersController";
 
 const usersRouter = express.Router();
 
