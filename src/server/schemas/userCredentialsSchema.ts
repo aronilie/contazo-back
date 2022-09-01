e7cc7597108093b2980b230503d38f6d9dd756cc
Aron Ilie <aronilie.code@gmail.com>
2022-09-01 17:51:46 +0200

    Added name, surname and email properties to joi object

--- src/server/schemas/userCredentialsSchema.ts
+++ src/server/schemas/userCredentialsSchema.ts
@@ -2,6 +2,9 @@ import { Joi } from "express-validation";
 
 const userCredentialsSchema = {
   body: Joi.object({
+    name: Joi.string().min(1),
+    surname: Joi.string().min(1),
+    email: Joi.string().min(1),
     phoneNumber: Joi.string().min(1).required(),
     password: Joi.string().min(1).required(),
   }),
