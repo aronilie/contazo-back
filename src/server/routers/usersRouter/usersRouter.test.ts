119d041fe9bcfe760f7cafbbe4359ddc61462092
Aron Ilie <aronilie.code@gmail.com>
2022-09-01 15:19:46 +0200

    Refactorized test router of login

--- src/server/routers/usersRouter/usersRouter.test.ts
+++ src/server/routers/usersRouter/usersRouter.test.ts
@@ -3,17 +3,22 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import app from "../..";
 import connectDB from "../../../database/index";
+import User from "../../../database/models/User/User";
 
 let server: MongoMemoryServer;
 
 beforeAll(async () => {
   server = await MongoMemoryServer.create();
-  const mongoURL = server.getUri();
+  const url = server.getUri();
+  await connectDB(url);
+});
 
-  await connectDB(mongoURL);
+afterEach(async () => {
+  await User.deleteMany({});
 });
 
 afterAll(async () => {
+  await User.deleteMany({});
   await mongoose.connection.close();
   await server.stop();
 });
@@ -22,13 +27,10 @@ describe("Given the POST endpoint /users/register", () => {
   describe("When it receives a request with phoneNumber '+65 782 22 45' and password 'admin'", () => {
     test("Then it should response with status 201 and the user data", async () => {
       const user = { phoneNumber: "+65 782 22 45", password: "admin" };
+      const route = "/users/register";
       const expectedStatus = 201;
 
-      const response = await request(app)
-        .post("/users/register")
-        .send(user)
-        .expect(expectedStatus);
-      expect(response.statusCode).toEqual(expectedStatus);
+      await request(app).post(route).send(user).expect(expectedStatus);
     });
   });
 });
@@ -37,13 +39,12 @@ describe("Given the POST endpoint /users/login", () => {
   describe("When it receives a request with phoneNumber '+65 782 22 45' and password 'admin'", () => {
     test("Then it should response with status 200 and the token created", async () => {
       const user = { phoneNumber: "+65 782 22 45", password: "admin" };
+      const userLogin = { phoneNumber: "+65 782 22 45", password: "admin" };
+      const route = "/users/login";
       const expectedStatus = 200;
 
-      const response = await request(app)
-        .post("/users/login")
-        .send(user)
-        .expect(expectedStatus);
-      expect(response.statusCode).toEqual(expectedStatus);
+      await request(app).post("/users/register").send(user).expect(201);
+      await request(app).post(route).send(userLogin).expect(expectedStatus);
     });
   });
 });
