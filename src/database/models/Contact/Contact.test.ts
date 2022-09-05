17b9e8dba644f5119e59082dfa56765d2a964e4a
Aron Ilie <aronilie.code@gmail.com>
2022-09-05 18:26:47 +0200

    Tested Contact model

---
+++ src/database/models/Contact/Contact.test.ts
@@ -0,0 +1,26 @@
+import Contact from "./Contact";
+
+describe("Given a Contact model", () => {
+  describe("When it is called", () => {
+    test("Then it should create a model with the properties name, surname, email, phoneNumber, image and owner", () => {
+      const expectedKeys = [
+        "name",
+        "surname",
+        "email",
+        "phoneNumber",
+        "image",
+        "owner",
+      ];
+      const keys = Object.keys(Contact.schema.paths);
+      const userAttributes = [
+        keys[0],
+        keys[1],
+        keys[2],
+        keys[3],
+        keys[4],
+        keys[5],
+      ];
+      expect(userAttributes).toStrictEqual(expectedKeys);
+    });
+  });
+});
