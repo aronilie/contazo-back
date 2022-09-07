c84e49247baa6eae360f6ac5139c8bf33e857483
Aron Ilie <aronilie.code@gmail.com>
2022-09-07 18:07:28 +0200

    Created JwtCustomPayload interface

--- src/interfaces/JwTPayload.ts
+++ src/interfaces/JwTPayload.ts
@@ -1,4 +1,11 @@
-export interface JwtPayload {
+import { Request } from "express";
+import { JwtPayload } from "jsonwebtoken";
+
+export interface JwtCustomPayload {
   id: string;
   phoneNumber: string;
+}
+
+export interface CustomRequest extends Request {
+  payload: JwtPayload;
 }
