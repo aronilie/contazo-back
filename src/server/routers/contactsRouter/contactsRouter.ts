c8ae6c9f24d1451fb0d0f5e2564e79c4e9df6c11
Aron Ilie <aronilie.code@gmail.com>
2022-09-09 16:51:01 +0200

    Added route 'create' to contactsRouter

--- src/server/routers/contactsRouter/contactsRouter.ts
+++ src/server/routers/contactsRouter/contactsRouter.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import userAuthentication from "../../../utils/userAuthentication/userAuthentication";
 import {
+  createContact,
   deleteContact,
   getContacts,
 } from "../../controllers/contactsController/contactsController";
@@ -10,5 +11,7 @@ const contactsRouter = express.Router();
 contactsRouter.get("/contacts", userAuthentication, getContacts);
 
 contactsRouter.delete("/delete/:id", userAuthentication, deleteContact);
+
+contactsRouter.post("/create", userAuthentication, createContact);
 
 export default contactsRouter;
