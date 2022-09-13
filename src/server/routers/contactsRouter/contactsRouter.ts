c7f3eddf0ecd8b9436335c8cb0e446854b607491
Aron Ilie <aronilie.code@gmail.com>
2022-09-13 12:33:29 +0200

    Added filestorage in create middleweare

--- src/server/routers/contactsRouter/contactsRouter.ts
+++ src/server/routers/contactsRouter/contactsRouter.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import multer from "multer";
+import path from "path";
 import userAuthentication from "../../../utils/userAuthentication/userAuthentication";
 import {
   createContact,
@@ -6,10 +8,26 @@ import {
   getContactByPhoneNumber,
   getContacts,
 } from "../../controllers/contactsController/contactsController";
+import fileStorage from "../../middleweares/filesStorage";
 
 const contactsRouter = express.Router();
 
+const upload = multer({
+  dest: path.join("public"),
+  limits: {
+    fileSize: 8000000,
+  },
+});
+
 contactsRouter.get("/contacts", userAuthentication, getContacts);
+
+contactsRouter.post(
+  "/create",
+  upload.single("file"),
+  fileStorage,
+  userAuthentication,
+  createContact
+);
 
 contactsRouter.get(
   "/contacts/:phoneId",
@@ -18,7 +36,5 @@ contactsRouter.get(
 );
 
 contactsRouter.delete("/delete/:phoneId", userAuthentication, deleteContact);
-
-contactsRouter.post("/create", userAuthentication, createContact);
 
 export default contactsRouter;
